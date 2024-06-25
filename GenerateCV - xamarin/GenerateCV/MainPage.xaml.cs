5803101d11c0a42b949428e81ef9eea007bce7c8
Arlyz123 <147183201+Arlyz123@users.noreply.github.com>
2024-06-25 18:11:10 -0500

    Add files via upload

---
+++ GenerateCV - xamarin/GenerateCV/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using GenerateCV;
+using System;
+using Xamarin.Forms;
+
+namespace GenerateCV
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void InformacionAgregada(object sender, EventArgs e)
+        {
+            var userNombre = entryNombre.Text;
+            var userFecha = entryFecha.Text;
+            var userOcupacion = entryOcupacion.Text;
+            var userTelefono = entryTelefono.Text;
+            var userEmail = entryEmail.Text;
+            var userAptitud = entryAptitud.Text;
+            var userExperiencia1 = entryExperiencia1.Text;
+            var userFormacion1 = entryFormacion1.Text;
+
+            await Navigation.PushAsync(new Page1(userNombre, userFecha, userOcupacion, userTelefono, userEmail, userAptitud, userExperiencia1, userFormacion1));
+        }
+    }
+}
---
+++ GenerateCV - xamarin/GenerateCV/Page1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GenerateCV
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1(string nombre, string fecha, string ocupacion, string telefono, string email, string aptitud, string experiencia1, string formacion1)
+        {
+            InitializeComponent();
+
+            labelNombre.Text = nombre + "\n" + ocupacion;
+            labelFecha.Text = "Fecha de nacimiento:\n" + fecha;
+            labelTelefono.Text = "Telefono:\n" + telefono;
+            labelEmail.Text = "Email:\n" + email;
+            labelAptitud.Text = "Soy un " + ocupacion + " titulado. Mi objetivo es ser útil otorgando mis conocimientos en el área dada. Soy " + aptitud + ".\n";
+            labelExperiencia.Text = "Experiencia Laboral:\n" + experiencia1 + "\n";
+            labelFormacion.Text = "Formación Académica:\n" + formacion1 + "\n";
+        }
+
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
